f9f86c9f5ebc69eca5a0ca9fcb1174c994a1ff16
Samantha <serick01@rams.shepherd.edu>
2025-11-10 22:51:07 -0500

    moved files & deleted duplicates

--- api/app/main.py
+++ api/app/main.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from api.app.security import get_current_user
 
 app = FastAPI(title="Beyond the Bookshelf - API", version="0.1.0")
 
 app.add_middleware(
-    CORSMiddleware, allow_origins=["http://localhost:3000"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 async def health():
-    return {"status":"ok"}
+    return {"status": "ok"}
+
+@app.get("/api/users/me")
+async def users_me(user = Depends(get_current_user)):
+    return user
