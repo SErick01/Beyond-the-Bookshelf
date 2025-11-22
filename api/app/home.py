ad3244a3c06fcbeec25b55a3be8a98d4cf8000d9
Samantha <serick01@rams.shepherd.edu>
2025-11-21 19:53:42 -0500

    more debugging

--- api/app/home.py
+++ api/app/home.py
@@ -246,22 +246,26 @@ async def get_pages_chart(year: int, user: dict = Depends(get_current_user)):
 
 
 @router.get("/stats/{year}/genres")
-async def get_genres_chart(year: int, user_id: str):
-    charts = create_yearly_charts(user_id=user_id, year=year)
+async def get_genres_chart(year: int, user: dict = Depends(get_current_user)):
+    if not user:
+        raise HTTPException(status_code=403, detail="Not authenticated")
+
+    charts = create_yearly_charts(user_id=user["id"], year=year)
 
     if not charts or "genres" not in charts:
         raise HTTPException(status_code=404, detail="No genre data for that year")
-
     return FileResponse(charts["genres"], media_type="image/png")
 
 
 @router.get("/stats/{year}/timeline")
-async def get_timeline_chart(year: int, user_id: str):
-    charts = create_yearly_charts(user_id=user_id, year=year)
+async def get_timeline_chart(year: int, user: dict = Depends(get_current_user)):
+    if not user:
+        raise HTTPException(status_code=403, detail="Not authenticated")
+    
+    charts = create_yearly_charts(user_id=user["id"], year=year)
 
     if not charts or "timeline" not in charts:
         raise HTTPException(status_code=404, detail="No completion data for that year")
-
     return FileResponse(charts["timeline"], media_type="image/png")
 
 
