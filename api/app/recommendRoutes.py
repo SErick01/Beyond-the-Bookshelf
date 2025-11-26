db047af5a5fcc727763c2f2a422463c254f21700
Samantha <serick01@rams.shepherd.edu>
2025-11-26 10:13:35 -0800

    removed unused routers and imports

--- api/app/recommendRoutes.py
+++ api/app/recommendRoutes.py
@@ -1,10 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter
 from pydantic import BaseModel
 from typing import List
-from .security import get_current_user
-from .recommendML.service import (
-    recommend_for_user, recommend_similar_works, 
-    recommend_newest_works, recommend_for_user_by_genre,)
+from .recommendML.service import (recommend_for_user, recommend_newest_works)
 
 router = APIRouter(prefix="/api/recommend", tags=["recommendations"])
 
@@ -24,23 +21,6 @@ def recommend_for_user_public(limit: int = 10):
     return recommend_for_user(user_id=demo_user_id, limit=limit)
 
 
-@router.get("/genre", response_model=List[WorkOut])
-def recommend_for_logged_in_user_by_genre(
-    genre: str,
-    limit: int = 10,
-    current_user: dict = Depends(get_current_user),
-):
-    if not current_user:
-        raise HTTPException(status_code=401, detail="Not authenticated")
-    
-    user_id = current_user["id"]
-    return recommend_for_logged_in_user_by_genre(user_id=user_id, genre=genre, limit=limit)
-
-
 @router.get("/newest", response_model=List[WorkOut])
 def recommend_newest(limit: int = 10):
     return recommend_newest_works(limit = limit)
-
-@router.get("/similar/{work_id}", response_model=List[WorkOut])
-def similar_works(work_id: str, limit: int = 10):
-    return recommend_similar_works(work_id=work_id, limit=limit)
